9b857122d16e3bda3dada9006824bb6cb702f0a5
Andrus Lall <andruslall@outlook.com>
2024-08-14 22:19:24 +0300

    feat: add nav menu for mobile

--- script.js
+++ script.js
@@ -22,4 +22,28 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelectorAll('.category').forEach(c => c.classList.remove('hidden'));
         });
     });
+
+    // Mobile menu open event
+    const menuIcon = document.querySelector('.mobile-menu-icon');
+    const sidenav = document.getElementById('mobileSidenav');
+    const closeBtn = sidenav.querySelector('.mobilenavclosebtn');
+
+    // Define the width of the sidenav in percentage
+    const sidenavWidth = '80%';
+
+    menuIcon.addEventListener('click', () => {
+        sidenav.style.width = sidenavWidth;
+    });
+
+    // Mobile menu close event
+    closeBtn.addEventListener('click', () => {
+        sidenav.style.width = '0';
+    });
+
+    // Optional: Close the sidenav if the user clicks outside of it
+    document.addEventListener('click', (event) => {
+        if (sidenav.style.width === sidenavWidth && !sidenav.contains(event.target) && event.target !== menuIcon) {
+            sidenav.style.width = '0';
+        }
+    });
 });
